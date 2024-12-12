6f0b99d085e84e477650f7917502d5bb66adff50
panto <panto@DESKTOP-2G72OCI>
2024-12-12 14:28:15 +0200

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace skhnh1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.BackgroundImage")));
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1192, 668);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click_1);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.pictureBox2.BackColor = System.Drawing.Color.DarkGreen;
+            this.pictureBox2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox2.BackgroundImage")));
+            this.pictureBox2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBox2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pictureBox2.Location = new System.Drawing.Point(962, 336);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(162, 332);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1192, 668);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace skhnh1
+{
+    public partial class Form1 : Form
+    {
+        // Δημιουργία PictureBox για τον χάρτη
+       
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.KeyPreview = true; // Ενεργοποίηση KeyPreview για να αναγνωρίζονται τα πλήκτρα
+
+            
+        }
+        private void pictureBox1_Click_1(object sender, EventArgs e)
+        {
+            // Λήψη των συντεταγμένων του σημείου που επιλέγει ο χρήστης
+            Point clickPoint = pictureBox1.PointToClient(MousePosition);
+
+            // Εμφάνιση δεδομένων για το σημείο που επιλέχθηκε
+            string data = GetPointData(clickPoint);
+            MessageBox.Show(data, "Πληροφορίες Σημείου", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+       
+        // Προσομοίωση δεδομένων για το σημείο (υγρασία, σταθερότητα, ήλιος)
+        private string GetPointData(Point point)
+        {
+            Random rand = new Random();
+
+            // Προσομοιωμένα δεδομένα για υγρασία, σταθερότητα, και ήλιο
+            int humidity = rand.Next(0, 101);  // Υγρασία 0-100%
+            int stability = rand.Next(0, 101); // Σταθερότητα εδάφους 0-100%
+            int sunlight = rand.Next(0, 101);  // Έκθεση στον ήλιο 0-100%
+
+            // Επιστροφή των δεδομένων για την περιοχή
+            return $"Σημείο: ({point.X}, {point.Y})\nΥγρασία: {humidity}%\nΣταθερότητα εδάφους: {stability}%\nΈκθεση στον ήλιο: {sunlight}%";
+        }
+
+        // Λειτουργία για να δημιουργήσουμε νέες φόρμες όταν πατηθεί το πλήκτρο M
+        protected override void OnKeyDown(KeyEventArgs e)
+        {
+            base.OnKeyDown(e);
+
+            // Έλεγχος αν το πλήκτρο είναι το 'M'
+            if (e.KeyCode == Keys.M)
+            {
+                // Παίρνουμε την ανάλυση της οθόνης
+                Rectangle screenBounds = Screen.PrimaryScreen.Bounds;
+
+                // Υπολογίζουμε τις διαστάσεις για τις δύο φόρμες (ίδιο μέγεθος, δίπλα-δίπλα)
+                int halfWidth = screenBounds.Width / 2;
+                int height = screenBounds.Height;
+
+                // Δημιουργία και ρύθμιση πρώτης φόρμας (αριστερή πλευρά)
+                Form form2 = new Form
+                {
+                    Text = "Form 2",
+                        Size = new Size(halfWidth, height),
+                        StartPosition = FormStartPosition.Manual,
+                    Location = new Point(0, 0) // Αριστερή πλευρά
+                };
+
+                // Δημιουργία και ρύθμιση δεύτερης φόρμας (δεξιά πλευρά)
+                Form form3 = new Form
+                {
+                    Text = "Form 3",
+                    Size = new Size(halfWidth, height),
+                    StartPosition = FormStartPosition.Manual,
+                    Location = new Point(halfWidth, 0) // Δεξιά πλευρά
+                };
+
+                // Εμφάνιση των φορμών
+                form2.Show();
+                form3.Show();
+            }
+        }
+
+      
+
+        
+    }
+}
